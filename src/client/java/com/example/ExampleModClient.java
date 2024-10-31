e1df7ee93945553b6b81a86dc9d373e6a2b6a16d
pheological <162748832+pheological@users.noreply.github.com>
2024-10-30 20:26:53 -0700

    Update ExampleModClient.java

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -4,7 +4,25 @@ import net.fabricmc.api.ClientModInitializer;
 
 public class ExampleModClient implements ClientModInitializer {
 	@Override
+public static final Logger LOGGER = LoggerFactory.getLogger("nojumpdelay");
+	int ticks = 0;
+
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ClientTickEvents.END_CLIENT_TICK.register((client) -> {
+			Random random = new Random();
+			++this.ticks;
+			MinecraftClient mc = MinecraftClient.getInstance();
+			if (mc.player != null) {
+				int randomNumber = random.nextInt(3) + 1;
+				if (this.ticks > randomNumber) {
+					if (mc != null && mc.player != null && client.player != null && mc.options.jumpKey.isPressed() && mc.player.isOnGround() && !mc.player.isCreative() && !mc.player.isSpectator()) {
+						mc.player.jump();
+					}
+
+					this.ticks = 0;
+				}
+			}
+
+		});
 	}
 }
